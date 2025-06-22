e6e301179937a1d77db78d8bf1bcacbbd21c9595
iarguealot <d35488631@gmail.com>
2025-06-22 13:18:07 +0200

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,3 @@
-```
 from discord import intents
 from discord.ext.commands import Bot
 
@@ -13,4 +12,3 @@ async def on_ready():
   print("my project is cool, the projects name is https://charmbot.life")
 
 bot.run("MTM4NjMwMjgwOTE2NjU4MTkxMQ.GHPi1r.TlBh1dUWtYeJX0huplK5ltM_0X5NF2-LxjQh8U") # the token is valid
-```
